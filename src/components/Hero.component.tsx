1156a5e22d6767d9dc42716c3bdd3b8afd278ac1
m0bar3z <amobarez00@gmail.com>
2023-06-22 19:41:24 +0330

    feat: typing animation for hero section

--- src/components/Hero.component.tsx
+++ src/components/Hero.component.tsx
@@ -1,11 +1,23 @@
+'use client';
+
 import { type FC } from 'react';
+import cn from 'classnames';
+import { useTypingAnimation, TypePhase } from '@/hooks/useTypingAnimatino';
 
 const Hero: FC = () => {
+  const { typedText, phase, selectedText } = useTypingAnimation(['a React dev', 'Carl.']);
+
   return (
     <section className='bg-[#4495D4] h-hero flex items-center justify-center'>
-      <div className='h-3/5 w-full'>
-        <h1 className='mb-2 text-4xl text-gray-100 md:text-6xl'>
-          Hi, I&apos;m <br className='block md:hidden' /> a React dev
+      <div className='h-2/5 w-full m-32'>
+        <h1
+          className={cn('mb-2 text-4xl text-gray-100 text-center md:text-7xl', {
+            ['end-cursor']: phase !== TypePhase.Deleting && phase !== TypePhase.Stopping,
+            ['blinking']: phase === TypePhase.Pausing,
+          })}
+          aria-label={selectedText}
+        >
+          Hi, I&apos;m {typedText}
         </h1>
       </div>
     </section>
---
+++ src/hooks/useTypingAnimatino.tsx
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+
+export enum TypePhase {
+  Typing,
+  Pausing,
+  Deleting,
+  Stopping,
+}
+
+const TYPING_INTERVAL = 200;
+const PAUSE_MS = 1500;
+const DELETING_INTERVAL = 100;
+
+export const useTypingAnimation = (
+  texts: string[]
+): {
+  typedText: string;
+  selectedText: string;
+  phase: number;
+} => {
+  const [phase, setPhase] = useState<TypePhase>(TypePhase.Typing);
+  const [typedText, setTypedText] = useState<string>('');
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  console.log(phase);
+  useEffect(() => {
+    switch (phase) {
+      case TypePhase.Typing: {
+        const nextTypedHeroTitle = texts[selectedIndex].slice(0, typedText.length + 1);
+        if (nextTypedHeroTitle === typedText) {
+          setPhase(TypePhase.Pausing);
+          return;
+        }
+        const timeout = setTimeout(() => {
+          setTypedText(nextTypedHeroTitle);
+        }, TYPING_INTERVAL);
+        return () => clearTimeout(timeout);
+      }
+
+      case TypePhase.Deleting: {
+        if (!typedText) {
+          setPhase(TypePhase.Typing);
+          setSelectedIndex(selectedIndex + 1);
+          return;
+        }
+        const remainingText = texts[selectedIndex].slice(0, typedText.length - 1);
+        const timeout = setTimeout(() => {
+          setTypedText(remainingText);
+        }, DELETING_INTERVAL);
+        return () => clearTimeout(timeout);
+      }
+
+      case TypePhase.Pausing:
+      default: {
+        if (selectedIndex === texts.length - 1) {
+          setPhase(TypePhase.Stopping);
+          return;
+        }
+        const timeout = setTimeout(() => {
+          setPhase(TypePhase.Deleting);
+        }, PAUSE_MS);
+        return () => clearTimeout(timeout);
+      }
+    }
+  }, [texts, typedText, phase, selectedIndex]);
+
+  return { typedText, selectedText: texts[selectedIndex], phase };
+};
