1c0421c9494bdf8ac0d7e29e9289dcbc0c9da1c6
m0bar3z <amobarez00@gmail.com>
2023-07-03 11:13:58 +0330

    fix: add z-index to header

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -4,7 +4,7 @@ import Section from './components/Section.component';
 
 const Header: FC = () => {
   return (
-    <header className='h-11 flex items-center w-100 sticky top-0'>
+    <header className='h-11 flex items-center w-100 sticky top-0 z-50'>
       <Section title='/hello' className='bg-[#4495D4]' />
       <Section title='/projects' className='bg-[#5B9E47]' />
       <Section title='/contact' className='bg-[#F1A031]' />
